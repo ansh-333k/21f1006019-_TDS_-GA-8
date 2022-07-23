2b7831da1e706b51de260fb1ae1b283725b31638
ANSH KUSHWAHA <96056993+ansh-333k@users.noreply.github.com>
2022-07-23 13:51:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 import streamlit as st
 
 st.write("""
-# Credit Card Approval Prediction App
-This app predicts the credit card approval probablity
+# Largest of 3 Given Numbers
 """)
+
+st.header('Enter 3 Numbers')
+
+num_1 = st.number_input("1st Number",step=1)
+num_2 = st.number_input("2nd Number",step=1)
+num_3 = st.number_input("3rd Number",step=1)
+
+st.subheader('Entered Numbers are -')
+st.write(list(num_1, num_2, num_3))
+
+max = None
+
+if (num_1 > num_2) and (num_1 > num_3):
+  max = num_1
+elif (num_2 > num_1) and (num_2 > num_3):
+  max = num_2
+else:
+  max = num_3
+
+st.write("""
+## Largest of the given 3 numbers is -
+""")
+st.write(max)
